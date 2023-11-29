e30f6fe11408e46a41b8d97b1fe5f245d688f9e2
Sanjay Mahabir <smahab13@my.centennialcollege.ca>
2023-11-29 03:14:51 -0500

    Basic Web socket counter functionality

--- server.js
+++ server.js
@@ -8,6 +8,55 @@
 var app = require('./app');
 var debug = require('debug')('week-4:server');
 var http = require('http');
+const WebSocket = require('ws');
+
+let connectedClients = [];
+let connections = {
+  count: { port: 8887 }
+}
+
+// Clients
+const wss = new WebSocket.Server({ port: '8999' }, () => console.log(`WS Server is listening at 8999`));
+
+wss.on('connection', ws => {
+  ws.on('message', data => {
+    if (ws.readyState !== ws.OPEN) return;
+    connectedClients.push(ws);
+  })
+
+});
+
+
+
+// Sensors
+Object.entries(connections).forEach(([key, settings]) => {
+  const connection = connections[key];
+  connection.sensors = {};
+
+  new WebSocket.Server({ port: settings.port }, () => console.log(`WS Server is listening at ${settings.port}`)).on('connection', (ws) => {
+    ws.on('message', data => {
+     
+        console.log(data.toString());
+      if (ws.readyState !== ws.OPEN) return;
+      if (typeof data === 'object') {
+        connection.sensors = data.toString();
+        // For a future video, taking care of video stream from ESP32 Cam
+      } else {
+        connection.sensors = data.toString();
+      }
+      connectedClients.forEach(client => {
+        client.send(data.toString());
+      });
+    });
+  });
+});
+
+
+
+
+
+
+
 
 /**
  * Get port from environment and store in Express.
@@ -83,7 +132,7 @@ function onError(error) {
  */
 
 function onListening() {
-  
+
   console.log(`----Connected to http://localhost:${port}`)
 
   var addr = server.address();
